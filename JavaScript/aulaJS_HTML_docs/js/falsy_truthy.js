54a4582cb37c1b36d8b1fd80bec663155b602bb9
giovic16 <giovanavictoria887@gmail.com>
2023-01-08 18:54:22 -0300

    Aula 4 – Conceitos de tipagem e casting

---
+++ JavaScript/aulaJS_HTML_docs/js/falsy_truthy.js
@@ -0,0 +1,51 @@
+// FALSY e TRUTHY- Valores que se traduzem em verdadeiro ou falso quando existe uma análise no contesto booleano
+
+// FALSY - abaixo exemplos de retorno FALSO
+if(false) {
+
+}
+
+if(null) {
+
+}
+
+if(undefined) {
+
+}
+
+if(0) {
+
+}
+
+if(NaN) {
+
+}
+
+if("") {
+
+}
+
+// TRUTHY - abaixo exemplos de retorno VERDADEIRO
+if(true) {
+
+}
+
+if([]) {
+
+}
+
+if({}) {
+
+}
+
+if('myVar') {
+
+}
+
+if(8) {
+
+}
+
+if(new Image()) {
+
+}
