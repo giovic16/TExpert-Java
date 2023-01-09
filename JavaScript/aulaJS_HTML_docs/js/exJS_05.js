2bb122211404f2c617ed044040be80e6182f5d5f
giovic16 <giovanavictoria887@gmail.com>
2023-01-08 22:08:47 -0300

    Aula 7 – Simulando o comportamento de um carrinho de compras – parte 2

--- JavaScript/aulaJS_HTML_docs/js/exJS_05.js
+++ JavaScript/aulaJS_HTML_docs/js/exJS_05.js
@@ -6,6 +6,13 @@ let carrinho = {
     item3 : {'nome':'Ovos com bacon', 'preco':31}
 }
 
-document.querySelector('.item-nome').innerText = carrinho['item1'].nome
-document.querySelector('.item-preco').innerText = `R$ ${carrinho['item1'].preco.toFixed(2)}`
+document.querySelector('.item-nome span').innerText = carrinho['item1'].nome
+document.querySelector('.item-preco span').innerText = `R$ ${carrinho['item1'].preco.toFixed(2)}`
 document.querySelector('.item-imagem').innerHTML = `<img src="./images/produtos/salad-gf22844a91_1920.jpg" alt="salada" class='w-50'/>`
+
+let inputNumber = document.querySelector('.input-item-qtd')
+
+inputNumber.addEventListener('change',() =>{
+    let total = carrinho['item1'].preco * inputNumber.value
+    document.querySelector('.item-total span').innerText=`R$ ${total.toFixed(2)}`
+})
