51d7d4ee3758b9c707f7e669f16322d9ca906d2c
giovic16 <giovanavictoria887@gmail.com>
2023-01-09 14:06:57 -0300

    Aula 11 – Imprimindo em Tela os dados de um Objeto

--- JavaScript/aulaJS_HTML_docs/js/exJS_07.js
+++ JavaScript/aulaJS_HTML_docs/js/exJS_07.js
@@ -1,9 +1,9 @@
 console.log('testando file js')
 
 let carrinho = {
-    item1 : {'nome':'Salada Mix', 'preco':12},
-    item2 : {'nome':'Carne Grelhada', 'preco':25},
-    item3 : {'nome':'Ovos com bacon', 'preco':31}
+    item1 : {'imagem':'salad-gf22844a91_1920.jpg', 'nome':'Salada Mix', 'preco':12},
+    item2 : {'imagem':'beef-g66eb2a540_1920.jpg', 'nome':'Carne Grelhada', 'preco':25},
+    item3 : {'imagem':'breakfast-ga9b591474_1920.jpg', 'nome':'Ovos com bacon', 'preco':31}
 }
 
 // Buscando uma ocorrência dentro de uma string
@@ -25,26 +25,40 @@ document.querySelector('#msgCarrinho').innerText = msg02
 
 
 
-document.querySelector('.item-nome span').innerText = carrinho['item1'].nome
-document.querySelector('.item-preco span').innerText = `R$ ${carrinho['item1'].preco.toFixed(2)}`
-document.querySelector('.item-imagem').innerHTML = `<img src="./images/produtos/salad-gf22844a91_1920.jpg" alt="salada" class='w-50'/>`
+// document.querySelector('.item-nome span').innerText = carrinho['item1'].nome
+// document.querySelector('.item-preco span').innerText = `R$ ${carrinho['item1'].preco.toFixed(2)}`
+// document.querySelector('.item-imagem').innerHTML = `<img src="./images/produtos/salad-gf22844a91_1920.jpg" alt="salada" class='w-50'/>`
 
-// Percorrendo o objeto-array
-// Exemplo 1
-for (const key of Object.keys(carrinho)) {
-    //console.log(key) // retorna o nome da variável
-    //console.log(carrinho[key].nome) // retorna o nome da variável + o nome dos itens detro dela
 
-}
-
-// Exemplo 2
-let total = 0
-Object.keys(carrinho).forEach((key)=>{
-    total = total + carrinho[key].preco
-    console.log(total)
+Object.keys(carrinho).forEach((key, i)=>{
+    // total = total + carrinho[key].preco
+    // console.log(total)
+    document.querySelector(`.item-carrinho-${i} .item-nome span`).innerText = carrinho[key].nome
+    document.querySelector(`.item-carrinho-${i} .item-imagem`).innerHTML = `<img src='./images/produtos/${carrinho[key].imagem}' alt="salada" class='w-75'/>`
+    document.querySelector(`.item-carrinho-${i} .item-preco span`).innerText = `R$ ${carrinho[key].preco.toFixed(2)}`
+    document.querySelector(`.item-carrinho-${i} .item-total span`).innerText = `R$ ${carrinho[key].preco.toFixed(2)}`
+    
+    
     console.log(key)
     console.log(carrinho[key].nome)
 })
+
+// Percorrendo o objeto-array
+// Exemplo 1
+// for (const key of Object.keys(carrinho)) {
+    //console.log(key) // retorna o nome da variável
+    //console.log(carrinho[key].nome) // retorna o nome da variável + o nome dos itens detro dela
+
+// }
+
+// Exemplo 2
+// let total = 0
+// Object.keys(carrinho).forEach((key)=>{
+//     total = total + carrinho[key].preco
+//     console.log(total)
+//     console.log(key)
+//     console.log(carrinho[key].nome)
+// })
 
 // Alternativa 1
 // var inputNumber = document.querySelector('.input-item-qtd')
@@ -64,9 +78,8 @@ Object.keys(carrinho).forEach((key)=>{
 //     console.log(e.target)  // no console aparece as interações realizadas na tela
 //     console.log(e) // retorna eventos no console
 // })
-
 // Alternativa 3
-var inputNumber = document.querySelector('.input-item-qtd')
+var inputNumber = document.querySelector('.item-carrinho-0 .input-item-qtd')
 
 inputNumber.onchange = function() {
     let total = carrinho['item1'].preco * this.value 
