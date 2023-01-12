69d3e673bbcd521e56505324d8e75f532cdb6190
giovic16 <giovanavictoria887@gmail.com>
2023-01-12 10:55:37 -0300

    Aula 26 – Programando a Integração da Tela Rodízio de Funcionários – Parte 3

--- JavaScript/aulaJS_HTML_docs/js/exJS_12.js
+++ JavaScript/aulaJS_HTML_docs/js/exJS_12.js
@@ -6,11 +6,11 @@ console.log('testando file js')
 import {trocaSetor, trocaTurno} from "./module/modulesAula11.js"
 
 let turno1Txt, turno2Txt, setor1Txt, setor2Txt, tipo1Txt, 
-    tipo2Txt, setor1, setor2, btnConfTurno, btnConfSetor, 
-    ativo, anterior, duplas, listOn, listOff
+    tipo2Txt, setor1, setor2, anterior, duplas, listOn, listOff,
+    infoT1, infoT2, infoS1, infoS2, infoTt1, infoTt2
 
 anterior = document.querySelector(`.dupla-0`)
-ativo = 0
+var ativo = 0
 
 duplas = {
     dupla0 : ['Estela Martins','Adonis Santos'],
@@ -18,11 +18,14 @@ duplas = {
     dupla2 : ['Francis Albieri','Portella Afonso Silva']
 }
 
-let infoT1 = document.querySelector(`.dupla${ativo} #info1-00 span:nth-of-type(1)`)
-let infoT2 = document.querySelector(`.dupla${ativo} #info1-00 span:nth-of-type(1)`)
-let infoS1 = document.querySelector(`.dupla${ativo} #info1-00 span:nth-of-type(2)`)
-let infoS2 = document.querySelector(`.dupla${ativo} #info1-00 span:nth-of-type(2)`)
-
+function infos(ativo) {
+    infoT1 = document.querySelector(`.dupla${ativo} #info1-00 span:nth-of-type(1)`)
+    infoT2 = document.querySelector(`.dupla${ativo} #info1-01 span:nth-of-type(1)`)
+    infoS1 = document.querySelector(`.dupla${ativo} #info1-00 span:nth-of-type(2)`)
+    infoS2 = document.querySelector(`.dupla${ativo} #info1-01 span:nth-of-type(2)`)   
+    infoTt1 = document.querySelector(`.dupla${ativo} #info1-00 span:nth-of-type(3)`)
+    infoTt2 = document.querySelector(`.dupla${ativo} #info1-01 span:nth-of-type(3)`)  
+}
 
 turno1Txt = document.querySelector('#func01-turno')
 turno2Txt = document.querySelector('#func02-turno')
@@ -32,25 +35,27 @@ tipo1Txt = document.querySelector('#func01-extra')
 tipo2Txt = document.querySelector('#func02-extra')
 
 
+infos(ativo)
 
 trocaTurno(turno1Txt, turno2Txt, infoT1, infoT2)
-
 document.querySelector("#btnConfTurno").addEventListener('click',function(){
+    infos(ativo)
     trocaTurno(turno1Txt, turno2Txt, infoT1, infoT2)
 }) 
 
 trocaSetor(setor1Txt, setor2Txt, infoS1, infoS2)
-
 document.querySelector("#btnConfSetor").addEventListener('click',function(){
+    infos(ativo)
     trocaSetor(setor1Txt, setor2Txt, infoS1, infoS2)
 }) 
 
+document.querySelector('#info1-00 span:nth-of-type(3)').innerText = extra1
+document.querySelector('#info1-00 span:nth-of-type(3)').innerText = extra2
+document.querySelector('#func01-extra').value = extra1
+document.querySelector('#func02-extra').value = extra2
+
 document.querySelector("#btnConfTipoTurno").addEventListener('click',function(){
-    var tempo = tipo1Txt.value
-    tipo1Txt.value = tipo2Txt.value
-    document.querySelector('#info1-00 span:nth-of-type(3)').innerText = tipo1Txt.value
-    tipo2Txt.value = tempo
-    document.querySelector('#info1-01 span:nth-of-type(3)').innerText = tipo2Txt.value
+    trocaTipoTurno(tipo1Txt, tipo2Txt, infoTt1, infoT2)
 }) 
 
 
@@ -64,9 +69,8 @@ document.querySelectorAll('input[type=radio]').forEach((n, i)=>{
         document.querySelector(`.dupla-${i}`).classList.add(...listOn)
         anterior.classList.remove(...listOn)
         anterior = document.querySelector(`.dupla-${i}`)
-        // anterior = i
         ativo = i
-        document.querySelector('label[for=func-01').innerText = duplas[`dupla${i}`][0]
-        document.querySelector('label[for=func-02').innerText = duplas[`dupla${i}`][1]
+        document.querySelector('label[for=func-01]').innerText = duplas[`dupla${i}`][0]
+        document.querySelector('label[for=func-02]').innerText = duplas[`dupla${i}`][1]
     }
 })
