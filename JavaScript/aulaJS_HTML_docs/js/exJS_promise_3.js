889f7109e997c506da77925187c637f10c68cade
giovic16 <giovanavictoria887@gmail.com>
2023-01-13 10:43:01 -0300

    Aula 41 – Palavras chave async / await

--- JavaScript/aulaJS_HTML_docs/js/exJS_promise_3.js
+++ JavaScript/aulaJS_HTML_docs/js/exJS_promise_3.js
@@ -13,3 +13,28 @@ const carregaTxt = new Promise(f_carregaTxt)
 
 carregaTxt.then(result =>{result})
 .catch(erro =>{console.log(erro)})
+
+//async await
+async function saudacao() {
+    const m = await 'Olá, tudo bem?'
+    return m
+}
+saudacao().then(result =>{console.log(result)})
+
+async function despedida(msg) {
+    const tchau = await msg
+    return msg
+}
+despedida('Estou indo').then(result =>{console.log(result)})
+
+function carregaEm5Segundos(x) {
+    return new Promise(resolve => {
+        setTimeout(()=>{resolve(x)}, 5000)
+    }
+)}
+
+async function verificaCarregamento() {
+    const x = await carregaEm5Segundos('Arquivo ok!')
+    console.log(x)
+}
+verificaCarregamento()
