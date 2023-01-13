7b042bdf88d1d37e2be5130739c43833a9c73881
giovic16 <giovanavictoria887@gmail.com>
2023-01-12 21:51:11 -0300

    Aula 34 – Gerando chaves únicas com o objeto Math – Parte 2

--- JavaScript/aulaJS_HTML_docs/js/exJS_14.js
+++ JavaScript/aulaJS_HTML_docs/js/exJS_14.js
@@ -23,6 +23,24 @@ console.log(depois)
 console.log(primeirasLetras)
 console.log(`${antesMask}${depois}`)
 document.querySelector('.p2').innerHTML = `Email: ${email} <br/> Email com máscara ${antesMask}${depois}`
-
 // let primeirasLetras = antes.slice(4) // remove as primeiras 4 letras do email
 // let antesMask = antes.padStart(antes.length)
+
+// MATH
+let nn = 13.4
+console.log(Math.sqrt(16))
+let paraBaixo = Math.floor(nn) // arredonda para 13
+let paraCima= Math.ceil(nn) // arredonda para 14
+let arredondamento = Math.round(nn) // <= 13.4 arredonda para 13 / >= 13.5 arredonda para 14
+console.log('Math.floor - Para o mais baixo valor', paraBaixo)
+console.log('Math.ceil - Para o mais alto valor', paraCima)
+console.log('Math.round - Arrendonda (para baixo até .49, além disso para cima)', arredondamento)
+
+let c = Math.random().toString(36).substring(2, 9) // gera 7 números com letras, ex: 9o6pq1blsgj
+console.log(c)
+
+document.querySelector('.p3').innerHTML = `O código é <b>${c}</b>`
+
+// let c = Math.random() * 100
+// let x = Math.random().toString(36) // gera números com letras com ponto, ex: 0.8rhqoeun69f
+// let x = Math.random().toString(36).substring(2) // gera números com letras removendo o ponto, ex: l4y83eyw8r
