ed21186ea1f5dc18317a5d9f91763f4d456ef2dc
giovic16 <giovanavictoria887@gmail.com>
2023-01-19 15:30:21 -0300

    Aula 5 – Primeiros Conceitos de Componentes em Vue

--- aulaVueArquivos/app.js
+++ aulaVueArquivos/app.js
@@ -3,9 +3,10 @@ import {createApp} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 createApp({ 
     data(){ 
         return{ 
-            msg: 'Bem vindo ao Vue Js!', 
-            msg2: 'Bora codar juntos',
-            count: 0 
+            msg: 'Bem vindo ao Vue Js!' 
             } 
         } 
-    }).mount('#app') 
+    })//.mount('#app') 
+
+    import calculos from './calculos.js'
+    createApp(calculos).mount('#app')
---
+++ aulaVueArquivos/calculos.js
@@ -0,0 +1,14 @@
+export default{
+    data(){ 
+        return{ 
+            msg2: 'Bora codar juntos',
+            count: 0,
+            n: 0 
+            } 
+        },
+        methods:{
+            multiplicar(){
+                this.n = this.count
+            }
+        } 
+}
