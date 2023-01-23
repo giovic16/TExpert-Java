fc5431f69c3eb5ef4058d3c43e504ef979321f7d
giovic16 <giovanavictoria887@gmail.com>
2023-01-23 19:14:09 -0300

    Aula 23 – Utilizando o Recurso

--- aulaVueArquivos/projeto_spa_teste/src/components/MenuImage.vue
+++ aulaVueArquivos/projeto_spa_teste/src/components/MenuImage.vue
@@ -2,12 +2,17 @@
     <NavBar />
     <p class="p-3"><a href="#">Aproveite nossas promoções</a></p>
     <!-- <img :src="require('@/assets/produtos/noodles-7390775_1920.jpg')" class="w-25" /> -->
-    <img :src="urlImage" :alt="altImage" class="w-25" />
+    <div class="border p-3 mb-5">
+        <img :src="urlImage" :alt="altImage" class="w-25" />
+        <h4 class="py-3">{{ altImage }}</h4>
+        <ButtonImage @trocarImagem="novaImagem" />
+    </div>
     
 </template>
 
 <script>
 import NavBar from './NavBar'
+import ButtonImage from './ButtonImage'
 export default {
     name: 'MenuImage',
     data() {
@@ -18,6 +23,13 @@ export default {
     },
     components: {
         NavBar,
+        ButtonImage
+    },
+    methods: {
+        novaImagem() {
+            this.urlImage = require('@/assets/produtos/taco-4709325_1920.jpg')
+            this.altImage = 'Tacos mexicanos'
+        }
     }
 }
 </script>
