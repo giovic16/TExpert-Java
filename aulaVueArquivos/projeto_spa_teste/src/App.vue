034f82600150a6780a091dde78694edea14b5206
giovic16 <giovanavictoria887@gmail.com>
2023-01-23 10:35:41 -0300

    Aula 13 – Criando um Novo Componente

---
+++ aulaVueArquivos/projeto_spa_teste/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <MeuComp />
+</template>
+
+<script>
+  import MeuComp from './components/MeuComp'
+  export default {
+    name: 'App',
+    components: {
+      MeuComp
+    }
+  }
+</script>
---
+++ aulaVueArquivos/projeto_spa_teste/src/components/MeuComp.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Bora codar com Vue3 JS!</h1>
+</template>
+
+<script>
+    export default {
+        name: 'MeuComp'
+    }
+</script>
