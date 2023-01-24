3e2758be757e0c8a0fc011d6db11641610ac247b
giovic16 <giovanavictoria887@gmail.com>
2023-01-23 21:55:14 -0300

    Aula 25 – Funções de Renderização e JSX – parte 2

--- aulaVueArquivos/projeto_spa_teste/src/components/SobreNos.vue
+++ aulaVueArquivos/projeto_spa_teste/src/components/SobreNos.vue
@@ -1,17 +1,26 @@
 <script>
 import { h } from 'vue'
-function sobreInfo(titulo){
-    const vnode = h('h3',{id:'tituloSobre', class:'border p-2 bg-warning text-white'}, titulo)
+function sobreInfo(titulo, url, texto){
+    const vnode = 
+    h('div',{class:'border p-2'},[
+        h('h3',{id:'tituloSobre', class:'bg-warning text-white px-3'}, titulo),
+        h('div',{class:'d-flex'}, [
+            h(<p class="destaque px-2">{texto}</p>),
+            h(<img src={url} class="w-50" />)
+        ])
+    ])
     return vnode
 }
 export default {
     data() {
         return{
-            titulo: 'Sobre nosso restaurante'
+            titulo: 'Sobre nosso restaurante',
+            urlImage: require('@/assets/chef.jpg'),
+            texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate rem dolorum quos id porro suscipit illum nobis nulla sunt.'
         }
     },
     render() {
-        return sobreInfo(this.titulo)
+        return sobreInfo(this.titulo, this.urlImage, this.texto)
     }
 }
 </script>
