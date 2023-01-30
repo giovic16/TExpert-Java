9c290cf288371c9c1ef4c746830d1463d284a055
giovic16 <giovanavictoria887@gmail.com>
2023-01-30 20:00:51 -0300

    Aula 11 – Conceitos de promoção e casting

---
+++ Java/src/TestaPromocao.java
@@ -0,0 +1,14 @@
+public class TestaPromocao {
+    public static void main(String[] args) {
+        int valor1 = 10;
+        int valor2 = 4;
+
+        int divisao = valor1 / valor2;
+
+        double d = divisao;
+        double d2 = 10000000000d;
+        int i = (int) d2;
+        System.out.println(d);
+        System.out.println(d2);
+    }
+}
