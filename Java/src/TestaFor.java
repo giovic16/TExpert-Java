b29a26564a59a141a80cbdf5677064ff6193f284
giovic16 <giovanavictoria887@gmail.com>
2023-01-30 20:23:41 -0300

    Aula 15 – Estrutura de repetição FOR

---
+++ Java/src/TestaFor.java
@@ -0,0 +1,8 @@
+public class TestaFor {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++){
+            System.out.println(i);
+        }
+        System.out.println("Finalizou o FOR");
+    }
+}
