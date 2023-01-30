243758c1290bf4d1ba1f9c161061cb3a43963f0e
giovic16 <giovanavictoria887@gmail.com>
2023-01-30 19:41:44 -0300

    Aula 9 – Conhecendo os tipos de variáveis

--- Java/src/TestaVariavel.java
+++ Java/src/TestaVariavel.java
@@ -5,6 +5,22 @@ public class TestaVariavel {
 		double preco = 20.5;
 		preco = preco + 1;
 		System.out.println(preco);
+		
+		// tipos primitivos - inteiros
+		byte b = 120;
+		short s = 12500;
+		int i = 10000000;
+		long l = 20000000000000000l;
+		
+		// tipos primitivos - decimais 
+		float f = 12.3f;
+		double d = 20.5;
+		
+		// tipos primitivos - character
+		char c = 'A';
+		
+		// tipos primitivos - booleano
+		boolean boo = false;
 
 	}
 }
