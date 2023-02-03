ae58ff71388f91119d116d8c0d419c4f298954da
giovic16 <giovanavictoria887@gmail.com>
2023-02-03 08:22:46 -0300

    Aula 51 – Estudando os Métodos da Classe String

---
+++ Java/src/estudos/TestaString.java
@@ -0,0 +1,21 @@
+package estudos;
+
+public class TestaString {
+    public static void main(String[] args) {
+        String nome = new String("Giovana");
+        String sobrenome = new String("Mendes");
+
+        System.out.println("Nome: " + nome);
+        System.out.println("A String está vazia? " + nome.isEmpty());
+        System.out.println("Nome em maiúsculas: " + nome.toUpperCase());
+        System.out.println("Nome em minúsculas: " + nome.toLowerCase());
+        System.out.println("Imprime a letra do nome de acordo com indice passado: " + nome.charAt(0));
+        System.out.println("Concatena nome com sobrenome: " + nome.concat(" " + sobrenome));
+        System.out.println("Verifica se existe um nome com os caracteres dentro de contains: " + nome.contains("Giov"));
+        System.out.println("Start: " + nome.startsWith("Gio"));
+        System.out.println("End: " + nome.endsWith("ana"));
+        System.out.println("Tamanho do nome: " + nome.length());
+        System.out.println("Troca um caracter por outro: " + nome.replace('i', 'e'));
+
+    }
+}
